00615e82cce794cbfe197b1c41cb82bb17f5bda1
David <davidspeers25@outlook.com>
2018-07-18 17:28:51 +0100

    Fixing Error

--- multiplayer.js
+++ multiplayer.js
@@ -1,19 +1,19 @@
-'use strict';
-
-const express = require('express');
-const SocketServer = require('ws').Server;
-const path = require('path');
-
-const PORT = process.env.PORT || 3000;
-const INDEX = path.join(__dirname, 'index.html');
-
-const server = express()
-  .use((req, res) => res.sendFile(INDEX) )
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`));
-
-const wss = new SocketServer({ server });
-
 exports.execute = function() {
+  'use strict';
+
+  const express = require('express');
+  const SocketServer = require('ws').Server;
+  const path = require('path');
+
+  const PORT = process.env.PORT || 3000;
+  const INDEX = path.join(__dirname, 'index.html');
+
+  const server = express()
+    .use((req, res) => res.sendFile(INDEX) )
+    .listen(PORT, () => console.log(`Listening on ${ PORT }`));
+
+  const wss = new SocketServer({ server });
+
   wss.on('connection', (ws) => {
     console.log('Client connected');
     //Inside here messages can be received (check https://www.npmjs.com/package/ws#sending-and-receiving-text-data for more)
